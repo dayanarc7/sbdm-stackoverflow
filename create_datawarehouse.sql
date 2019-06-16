d4bef566ca8da883f1b192fc0dd34d833fd7a7e3
Dayana Román <daromanca@unal.edu.co>
2019-06-15 22:45:59 -0500

    Create create_datawarehouse.sql

---
+++ create_datawarehouse.sql
@@ -0,0 +1,20 @@
+SELECT * INTO dw_stackoverflow
+FROM (
+	SELECT año, pais, experiencia, ingresos, lenguaje_java, lenguaje_javascript, lenguaje_php, lenguaje_python, lenguaje_ruby, lenguaje_sql, lenguaje_c#, [lenguaje_c++], ocupacion_Desktop, ocupacion_Staff, ocupacion_Database, ocupacion_Embedded, ocupacion_Web
+	FROM m2011
+	UNION ALL
+	SELECT año, pais, experiencia, ingresos, lenguaje_java, lenguaje_javascript, lenguaje_php, lenguaje_python, lenguaje_ruby, lenguaje_sql, lenguaje_c#, [lenguaje_c++], ocupacion_Desktop, ocupacion_Staff, ocupacion_Database, ocupacion_Embedded, ocupacion_Web
+	FROM m2012
+	UNION ALL
+	SELECT año, pais, experiencia, ingresos, lenguaje_java, lenguaje_javascript, lenguaje_php, lenguaje_python, lenguaje_ruby, lenguaje_sql, lenguaje_c#, [lenguaje_c++], ocupacion_Desktop, ocupacion_Staff, ocupacion_Database, ocupacion_Embedded, ocupacion_Web
+	FROM m2013
+	UNION ALL
+	SELECT año, pais, experiencia, ingresos, lenguaje_java, lenguaje_javascript, lenguaje_php, lenguaje_python, lenguaje_ruby, lenguaje_sql, lenguaje_c#, [lenguaje_c++], ocupacion_Desktop, ocupacion_Staff, ocupacion_Database, ocupacion_Embedded, ocupacion_Web
+	FROM m2014
+	UNION ALL
+	SELECT año, pais, experiencia, ingresos, lenguaje_java, lenguaje_javascript, lenguaje_php, lenguaje_python, lenguaje_ruby, lenguaje_sql, lenguaje_c#, [lenguaje_c++], ocupacion_Desktop, ocupacion_System, ocupacion_Database, ocupacion_Embedded, ocupacion_Web
+	FROM m2015
+	UNION ALL
+	SELECT año, pais, experiencia, ingresos, lenguaje_java, lenguaje_javascript, lenguaje_php, lenguaje_python, lenguaje_ruby, lenguaje_sql, lenguaje_c#, [lenguaje_c++], ocupacion_Desktop, ocupacion_System, ocupacion_Database, ocupacion_Embedded, ocupacion_Web
+	FROM m2016
+) t
