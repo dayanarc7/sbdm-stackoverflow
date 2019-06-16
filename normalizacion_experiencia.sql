465a935e2fe3ef3b6e643e3c6f8cd8d0adbf5ece
José David Restrepo Duque <jodrestreopdu@unal.edu.co>
2019-06-16 02:17:35 -0500

    Corrección experiencia

--- normalizacion_experiencia.sql
+++ normalizacion_experiencia.sql
@@ -1,34 +1,35 @@
 ALTER TABLE dw_stackoverflow DROP CONSTRAINT IF EXISTS FK_experiencia
 DROP TABLE IF EXISTS experiencia
+GO
 
--- CREACIÓN TABLA TIEMPO
-
+-- CREACIÓN TABLA EXPERIENCIA
 CREATE TABLE [dbo].[experiencia](
 	[id_experiencia] [int] IDENTITY(1,1) NOT NULL,
-	[rango] [NVARCHAR] (max) NULL,
+	[rango] [NVARCHAR] (max) NOT NULL,
 	 CONSTRAINT [PK_experiencia] PRIMARY KEY CLUSTERED 
 	(
 		[id_experiencia] ASC
 	)
 )
 
--- NORMALIZACIÓN TIEMPO
+-- NORMALIZACIÓN EXPERIENCIA
 INSERT INTO experiencia
 SELECT DISTINCT experiencia
 FROM dw_stackoverflow
+WHERE experiencia IS NOT NULL
 
 -- ACTUALIZACIÓN DE LA LLAVE FORÁNEA
 UPDATE s
-SET s.fk_experiencia = t.id_experiencia
+SET s.fk_experiencia = e.id_experiencia
 --SELECT *
 FROM dw_stackoverflow s
-INNER JOIN experiencia t ON t.rango = s.experiencia
+INNER JOIN experiencia e ON e.rango = s.experiencia
 
 -- CONFIGURACIÓN LLAVE FORÁNEA
 ALTER TABLE dw_stackoverflow 
 ADD CONSTRAINT FK_experiencia
 FOREIGN KEY (fk_experiencia) REFERENCES experiencia(id_experiencia)
 
--- ELIMINACIÓN COLUMNA TIEMPO
+-- ELIMINACIÓN COLUMNA EXPERIENCIA
 ALTER TABLE dw_stackoverflow
 DROP COLUMN IF EXISTS experiencia
