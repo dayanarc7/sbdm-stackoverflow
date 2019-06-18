74bc2645a2f832efc7533a0e0e9613463971acb0
Dayana Román <daromanca@unal.edu.co>
2019-06-17 21:20:58 -0500

    Create Preguntas4567_lenguajes_x_pais_x_año - copia.sql

---
+++ Consultas_preguntas/Preguntas4567_lenguajes_x_pais_x_año - copia.sql
@@ -0,0 +1,14 @@
+-- PREGUNTA 4: ¿Cuál ha sido el comportamiento histórico de cada lenguaje en Colombia por país?
+
+SELECT u.pais, t.año, d.Java, d.Python, d.C#, d.[C++], d.Javascript, d.Php, d.Ruby, d.Sql 
+FROM
+(
+	SELECT d.FK_tiempo, d.FK_ubicacion, SUM(lenguaje_java) [Java], SUM(lenguaje_python) [Python], SUM(lenguaje_c#) [C#], SUM([lenguaje_c++]) [C++],
+			SUM(lenguaje_javascript) [Javascript], SUM(lenguaje_php) [Php], SUM(lenguaje_ruby) [Ruby], SUM(lenguaje_sql) [Sql]
+	FROM dw_stackoverflow d
+	GROUP BY d.FK_tiempo, d.FK_ubicacion
+) d
+INNER JOIN tiempo t ON t.id_tiempo = d.FK_tiempo
+INNER JOIN ubicacion u ON u.id_ubicacion = d.FK_ubicacion
+WHERE pais = 'Colombia'
+ORDER BY u.pais, t.año
